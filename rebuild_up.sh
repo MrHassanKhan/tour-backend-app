e1b0f8ea13473f9e9a0ba8a4fb706ae4389ab929
Muhammad Hassan <muhassan@elm.sa>
2024-02-25 15:11:12 +0300

    tchanged

--- rebuild_up.sh
+++ rebuild_up.sh
@@ -4,7 +4,7 @@
 git pull origin main
 
 # Step 2: Rebuild Docker containers
-sudo docker compose build
+sudo docker-compose build
 
 # Step 3: Bring up Docker Compose services
-sudo docker compose up -d
+sudo docker-compose up -d
